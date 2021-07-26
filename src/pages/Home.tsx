1c5deb7afc22da6136fd95150253f1ba7ae6abea
adsonnovaes <adsonnovaes7@gmail.com>
2021-07-26 19:26:21 -0300

    Refactoring: code cleanup and hook creation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -6,14 +5,14 @@ import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
-import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function Home() {
 
   const history = useHistory();
-  const  { user, signInWithGoogle } = useContext(AuthContext)
+  const  { user, signInWithGoogle } = useAuth();
 
   async function handlerCreateRoom() {
 
